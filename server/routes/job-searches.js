caf0b1787da43e1db1e2db81e026eee047731e21
Daniel Duarte <danieldd.ar@gmail.com>
2021-02-11 17:59:16 -0300

    feat(memory-repository): create in-memory repository to store job searches

--- server/routes/job-searches.js
+++ server/routes/job-searches.js
@@ -1,29 +1,57 @@
 var express = require('express');
+var { SearchJobRepository } = require('../repositories/search-job-repository');
 var router = express.Router();
 
+var repository = new SearchJobRepository();
+
 // Get the list of job searches.
-router.get('/', function(req, res, next) {
-  res.send('GET /job-searches');
+router.get('/', function(req, res) {
+  try {
+    const entities = repository.findAll();
+    res.json(entities);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 });
 
 // Create a job search.
-router.post('/', function(req, res, next) {
-  res.send('POST /job-searches');
+router.post('/', function(req, res) {
+  try {
+    repository.create(req.body);
+    res.end();
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 });
 
 // Get a job search by id.
-router.get('/:jsId', function(req, res, next) {
-  res.send('GET /job-searches/:jsId');
+router.get('/:jsId', function(req, res) {
+  try {
+    const entity = repository.findById(req.params.jsId);
+    res.json(entity);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 });
 
 // Update a job search by id.
-router.patch('/:jsId', function(req, res, next) {
-  res.send('PATCH /job-searches/:jsId');
+router.patch('/:jsId', function(req, res) {
+  try {
+    repository.updateById(req.params.jsId, req.body);
+    res.end();
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 });
 
 // Delete a job search by id.
-router.delete('/:jsId', function(req, res, next) {
-  res.send('DELETE /job-searches/:jsId');
+router.delete('/:jsId', function(req, res) {
+  try {
+    repository.deleteById(req.params.jsId);
+    res.end();
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 });
 
 module.exports = router;
