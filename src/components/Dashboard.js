e9fac55db019354b1310d32a3122ab33542af68c
Daniel Duarte <danieldd.ar@gmail.com>
2021-02-17 18:01:56 -0300

    feat(applications-listing): get job search by state active

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,33 +2,34 @@ import React, { useEffect, useState } from 'react';
 import DashboardList from './DashboardList';
 
 const Dashboard = () => {
-  
+
   const [ jobList, setJobList ] = useState([]);
   const [ jobTitle, setJobtTitle ] = useState('');
 
-  // @todo parameterize URL and IDs
   useEffect(() => {
-    fetch('http://localhost:5001/job-searches/602b3d082b20de73fed6fa35').then(
+    fetch('http://localhost:5001/job-searches?state=active').then(
       response => response.json()
     ).then(responseData => {
-      setJobtTitle(responseData.title)
-    });
-  }, [setJobtTitle]); 
-
-  useEffect(() => {
-    fetch('http://localhost:5001/job-searches/602b3d082b20de73fed6fa35/applications').then(
-      response => response.json()
-    ).then(responseData => {
-      const loadedJobList = [];
-      for (const key in responseData) {
-        loadedJobList.push({
-          id: key,
-          company: responseData[key].company,
+      // @todo Check how to choose when there are more than one job search active (now it chooses the 1st one)
+      const jobSearch = responseData.length > 0 ? responseData[0] : {};
+      if (jobSearch._id) {
+        fetch(`http://localhost:5001/job-searches/${jobSearch._id}/applications`).then(
+          response => response.json()
+        ).then(responseData => {
+          const loadedJobList = [];
+          for (const key in responseData) {
+            loadedJobList.push({
+              id: key,
+              company: responseData[key].company,
+            });
+          }
+          setJobList(loadedJobList)
         });
       }
-      setJobList(loadedJobList)
+      setJobtTitle(jobSearch.title)
     });
-  }, []); 
+
+  }, [setJobtTitle]);
 
   return (
       <div className="App">
